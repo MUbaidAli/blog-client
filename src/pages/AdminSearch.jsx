c92109aa900130a15a13595ee6d2d822aa7516fe
Ubaid Saleem <107278476+MUbaidAli@users.noreply.github.com>
2025-04-12 02:37:04 +0500

    Update AdminSearch.jsx

--- src/pages/AdminSearch.jsx
+++ src/pages/AdminSearch.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext.jsx";
 import { useNavigate } from "react-router-dom";
 import Pagination from "../components/Pagination";
 import API from "../utils/axiosInstance";
