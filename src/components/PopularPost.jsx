e18543c1aebab90435c4c1f8b43e77a59c0b78d0
Ubaid Saleem <107278476+MUbaidAli@users.noreply.github.com>
2025-04-17 02:08:11 +0500

    Update PopularPost.jsx

--- src/components/PopularPost.jsx
+++ src/components/PopularPost.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./SectionHeading";
+import svg1 from "../assets/vector3.svg";
 
 function PopularPost({ data }) {
   const [counter, setCounter] = useState(0);
@@ -15,9 +16,9 @@ function PopularPost({ data }) {
   }
 
   return (
-    <section className="mx-auto max-w-7xl px-6 sm:px-10 lg:px-8 py-10">
-      <section className="mx-auto max-w-5xl px-6 sm:px-10 lg:px-8 py-10">
-        <div className="md:w-150">
+    <section className="mx-auto max-w-7xl px-4 sm:px-10 lg:px-8 py-10">
+      <section className="mx-auto max-w-5xl px-4 sm:px-10 lg:px-8 py-10">
+        <div className="w-full md:w-150">
           <SectionHeading
             heading={"Most Popular Posts"}
             text={
@@ -31,7 +32,7 @@ function PopularPost({ data }) {
       <div className="slider flex sm:justify-between justify-center items-center relative overflow-hidden">
         {/* Left Button */}
         <img
-          src="/public/vector3.svg"
+          src={svg1}
           alt="left button"
           className="cursor-pointer rotate-180 mx-5 hidden sm:block"
           onClick={handleLeft}
@@ -51,14 +52,14 @@ function PopularPost({ data }) {
             alt=""
             className="m-5  h-60 w-80 object-cover"
           />
-          <div className="slider-text m-5 md:w-110 w-70">
+          <div className="slider-text m-0 sm:m-5 md:w-110 w-full">
             <div className="heading-text-section pb-8 pt-9">
               <h1
-                className={`mb-4  h-[60px] text-gray-900 dark:text-white  text-5xl sm:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#FF4242] to-[#99286C]`}
+                className={`mb-4  h-full text-gray-900 dark:text-white  text-5xl sm:text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#FF4242] to-[#99286C]`}
               >
                 {data[counter]?.title}
               </h1>
-              <p className={`text-white md:w-full md:text-1xl `}>
+              <p className={`text-white w-full text-1xl `}>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Consequuntur ipsa asperiores vel vero iusto error quae, est quis
                 doloremque vitae voluptatem commodi corrupti unde neque eveniet
@@ -70,7 +71,7 @@ function PopularPost({ data }) {
 
         {/* Right Button */}
         <img
-          src="/public/vector3.svg"
+          src={svg1}
           alt="right button"
           className="mx-5 hidden sm:block cursor-pointer"
           onClick={handleRight}
