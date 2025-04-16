a90987a97f457db167c5524a9341f48f37c9af1f
Ubaid Saleem <107278476+MUbaidAli@users.noreply.github.com>
2025-04-17 01:32:10 +0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,16 +6,16 @@ function HeroSection({ heading, text, btnText }) {
     <>
       <div className="h-[100vh] overflow-hidden relative">
         <img src={bgShadow} alt="" className="absolute flex w-full  " />
-        <div className="hero-sec container mx-auto max-w-7xl overflow-hidden  relative">
+        <div className="hero-sec container mx-auto max-w-7xl overflow-hidden  relative mt-10">
           <div className="circle inner"></div>
-          <div className="text-section absolute z-10  md:w-[600px] w-100  px-5 text-center flex items-center flex-col mt-10">
-            <h1 className="mb-4  text-gray-900 dark:text-white text-5xl md:text-7xl text-transparent bg-clip-text bg-gradient-to-r from-[#FF4242] to-[#99286C]">
+          <div className="text-section absolute z-10  sm:w-[600px] w-full  px-5 text-center flex items-center flex-col mt-10">
+            <h1 className="mb-4  leading-none text-gray-900 dark:text-white text-5xl   md:text-7xl text-transparent bg-clip-text bg-gradient-to-r from-[#FF4242] to-[#99286C]">
               {heading}
             </h1>
             <p className="my-3 text-white inline-block text-left text-1xl">
               {text}
             </p>
-            <div className="w-[350px]">
+            <div className="w-full sm:w-100">
               <Button>{btnText}</Button>
             </div>
           </div>
