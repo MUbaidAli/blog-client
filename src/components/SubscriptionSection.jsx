77c30c9138236826d38c43a1548368d1ea2bda4d
Ubaid Saleem <107278476+MUbaidAli@users.noreply.github.com>
2025-04-18 21:47:18 +0500

    Update SubscriptionSection.jsx

--- src/components/SubscriptionSection.jsx
+++ src/components/SubscriptionSection.jsx
@@ -4,18 +4,19 @@ import SubscriptionForm from "./SubscribtionForm";
 function SubscriptionSection() {
   return (
     <>
-      <div className="mx-auto max-w-7xl md:px-5 px-10 py-40">
+      <div className="mx-auto max-w-7xl md:px-5 px-3 py-40">
         <div className="flex flex-col justify-center items-center">
           <div className="w-full md:w-200">
             <SectionHeading
-              heading={"Join Our Wellness Community!"}
+              heading={"Join Our Wellness Community"}
               text={
                 "Get weekly health tips, exclusive content, and free wellness guides directly in your inbox"
               }
               headingSize={9}
             />
-
-            <SubscriptionForm />
+            <div className=" sm:w-full">
+              <SubscriptionForm />
+            </div>
           </div>
         </div>
       </div>
