0c3703165db5da22d63c959428586ac95ebf978a
Ubaid Saleem <107278476+MUbaidAli@users.noreply.github.com>
2025-04-18 23:53:20 +0500

    Update CategoryCard.jsx

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -4,7 +4,8 @@ function CategoryCard({ src, heading, text }) {
       <div className="p-4 w-100 my-5 mx-5 md:w-100 xl:w-70 border rounded-md  custom-border relative border-white">
         <div className="px-4 py-4 transform transition duration-500 hover:scale-110">
           <div className="flex justify-left w-20">
-            {src && <img src={`${src}`} className="w-32 mb-3" />}
+{/*             {src && <img src={`${src}`} className="w-32 mb-3" />} */}
+            <i class="fa-solid fa-icons text-white"></i>
           </div>
           <h2 className="text-left font-regular text-2xl text-white">
             {heading}
